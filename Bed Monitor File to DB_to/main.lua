c547084041c8a76aac81bd32418401268a857f34
unknown <eliot.muir@interfaceware.com>
2014-04-23 22:10:32 -0400

    Changed name of sqlite database - sqlite extension not needed.
    Put in some TODO comments.

--- Bed Monitor File to DB_to/main.lua
+++ Bed Monitor File to DB_to/main.lua
@@ -1,3 +1,4 @@
+-- TODO - could do with real sample data.
 
 function main(Data)
    local Hl7 = hl7.parse{vmd='example/demo.vmd', data=Data}
@@ -5,6 +6,7 @@ function main(Data)
    local Db = connect()
    
    if Hl7.MSH[9][1]:nodeValue() == 'ADT' then
+      -- TODO - can we catch real discharges?
       -- Every one out of four is a discharge
       if math.random(4) < 4 then
          mapPatient(T, Hl7.PID)
@@ -20,6 +22,7 @@ function main(Data)
    end
 end
 
+-- TODO pass in T.bed[1] as first argument.
 function dischargePatient(T, PV1)
    T.bed[1].patient_first_name = ""
    T.bed[1].patient_last_name = ""
@@ -27,20 +30,24 @@ function dischargePatient(T, PV1)
    mapVisit(T, PV1)
 end
 
+-- TODO pass in T.bed[1] as first argument.
 function mapPatient(T, PID)
    T.bed[1].patient_first_name = PID[5][1][2]:nodeValue()
    T.bed[1].patient_last_name = PID[5][1][1][1]:nodeValue()
 end
 
+-- TODO pass in T.bed[1] as first argument.
 function mapVisit(T, PV1)
    T.bed[1].ward = PV1[3][1]:nodeValue()
    T.bed[1].bed_name = PV1[3][3]
 end
 
+-- TODO pass in T.bed[1] as first argument.
 function mapObservation(T, OBX)
    T.bed[1].condition = OBX[3][5][1][2]   
 end
 
+-- TODO - share this database connection in common module with the GUI
 function connect()
-   return db.connect{api=db.SQLITE, name='bedmonitor.sqlite', live=true}
+   return db.connect{api=db.SQLITE, name='bedmonitor', live=true}
 end
