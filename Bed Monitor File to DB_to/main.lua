f5494c10397aa4da7a16195c8af9d2d48f260a18
Jonathan Marshall <jonathan.marshall@interfaceware.com>
2014-04-24 14:21:05 -0400

    Bed Monitor File to DB: Add proper check for
    patient discharge. Added sample messages for feeding
    GUIs database with ADTs.

--- Bed Monitor File to DB_to/main.lua
+++ Bed Monitor File to DB_to/main.lua
@@ -1,4 +1,4 @@
--- TODO - could do with real sample data.
+require 'db.sqlite'
 
 function main(Data)
    local Hl7 = hl7.parse{vmd='example/demo.vmd', data=Data}
@@ -6,14 +6,13 @@ function main(Data)
    local Db = connect()
    
    if Hl7.MSH[9][1]:nodeValue() == 'ADT' then
-      -- TODO - can we catch real discharges?
-      -- Every one out of four is a discharge
-      if math.random(4) < 4 then
-         mapPatient(T, Hl7.PID)
-         mapVisit(T, Hl7.PV1)
-         mapObservation(T, Hl7.OBX)
+
+      if Hl7.MSH[9][2]:nodeValue() == 'A03' then
+         dischargePatient(T.bed[1], Hl7.PV1)
       else
-         dischargePatient(T, Hl7.PV1)
+         mapPatient(T.bed[1], Hl7.PID)
+         mapVisit(T.bed[1], Hl7.PV1)
+         mapObservation(T.bed[1], Hl7.OBX)
       end
       
       trace(tostring(T))
@@ -22,32 +21,23 @@ function main(Data)
    end
 end
 
--- TODO pass in T.bed[1] as first argument.
 function dischargePatient(T, PV1)
-   T.bed[1].patient_first_name = ""
-   T.bed[1].patient_last_name = ""
-   T.bed[1].condition = ""
+   T.patient_first_name = ""
+   T.patient_last_name = ""
+   T.condition = ""
    mapVisit(T, PV1)
 end
 
--- TODO pass in T.bed[1] as first argument.
 function mapPatient(T, PID)
-   T.bed[1].patient_first_name = PID[5][1][2]:nodeValue()
-   T.bed[1].patient_last_name = PID[5][1][1][1]:nodeValue()
+   T.patient_first_name = PID[5][1][2]:nodeValue()
+   T.patient_last_name = PID[5][1][1][1]:nodeValue()
 end
 
--- TODO pass in T.bed[1] as first argument.
 function mapVisit(T, PV1)
-   T.bed[1].ward = PV1[3][1]:nodeValue()
-   T.bed[1].bed_name = PV1[3][3]
+   T.ward = PV1[3][1]:nodeValue()
+   T.bed_name = PV1[3][3]
 end
 
--- TODO pass in T.bed[1] as first argument.
 function mapObservation(T, OBX)
-   T.bed[1].condition = OBX[3][5][1][2]   
-end
-
--- TODO - share this database connection in common module with the GUI
-function connect()
-   return db.connect{api=db.SQLITE, name='bedmonitor', live=true}
+   T.condition = OBX[3][5][1][2]   
 end
