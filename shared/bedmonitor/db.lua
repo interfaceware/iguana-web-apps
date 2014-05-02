bf0dbd9df30a8d6207ea3ae6a6dd7fa2c9d87b7d
Jonathan Marshall <jonathan.marshall@interfaceware.com>
2014-05-01 22:38:46 -0400

    Bed Monitor: Datatable starts with 10 rows now.
    Uses a rather ugly but efficient sqlite style row constructor

--- shared/bedmonitor/db.lua
+++ shared/bedmonitor/db.lua
@@ -13,6 +13,13 @@ function bedmonitor.db.init()
    local Result = Connection:query{sql='SELECT * FROM sqlite_master WHERE type="table" and name="bed"', live=true}
    if #Result == 0 then
       Connection:execute{sql="CREATE TABLE bed (bed_name TEXT(255) NOT NULL ,ward TEXT(255),patient_first_name TEXT(255),patient_last_name TEXT(255),condition TEXT(255),PRIMARY KEY (bed_name));"}
+      local Sql = "INSERT INTO bed SELECT '101' AS bed_name, 'emergency_room' AS ward, '' AS patient_first_name, '' AS patient_last_name, '' AS condition"
+      for i=101, 109 do
+         Sql = Sql .. " UNION SELECT '" .. i .. "', 'emergency_room', '', '', ''"
+      end
+
+      trace(Sql)
+      Connection:execute{sql=Sql}
    end
    Connection:close()
 end
