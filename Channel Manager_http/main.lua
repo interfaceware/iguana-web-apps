19c82d6159722b05178e2accb4111057075dd561
eliotmuir <eliotmuir@users.noreply.github.com>
2014-05-09 21:51:11 -0400

    Fixed typo in my typo fix...

--- Channel Manager_http/main.lua
+++ Channel Manager_http/main.lua
@@ -14,7 +14,7 @@ function main(Data)
          default='app/cm/index.html',
          -- If the test property is defined then static files are pulled from the sandbox 
          -- rather than from the mile-stoned versioned copies of the files.  In production
-         -- the test property should be commmnted out.
+         -- the test property should be commented out.
          -- test='admin'    
       }   
       
