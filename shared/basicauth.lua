6600ecd5002f1bb4f054a509206f925d45c509b5
eliot.muir <eliot.muir@interfaceware.com>
2014-05-10 04:10:25 -0400

    Got basicauth module to follow naming convention.
    Also reduced if statement to a more compact form.
    
    Removed whitespace.

--- shared/basicauth.lua
+++ shared/basicauth.lua
@@ -1,47 +1,43 @@
-
 require 'stringutil'
 
-local BasicAuth = {}
+-- Notice that the namespace for the module matches the module name - i.e. basicauth
+-- When we use it within the code it is desirable to do:
+-- basicauth = require 'basicauth'
+-- Since this keeps the name of the module very consistent.
+local basicauth = {}
 
 local function getCreds(Headers)
    if not Headers.Authorization then
       return false
    end
-   
    local Auth64Str = Headers.Authorization:sub(#"Basic " + 1)
    local Creds = filter.base64.dec(Auth64Str):split(":")
    return {username=Creds[1], password=Creds[2]}
 end
 
-function BasicAuth.isAuthorized(Request)
+function basicauth.isAuthorized(Request)
    local Credentials = getCreds(Request.headers)
    if not Credentials then
       return false
    end
-   
    -- webInfo requires Iguana 5.6.4 or above
    local WebInfo = iguana.webInfo()
    local Status, Code = net.http.post{
       url=WebInfo.ip..":"..WebInfo.web_config.port.."/status",
       auth=Credentials,
       live=true}
-   
-   if Code == 200 then
-      return true
-   else
-      return false
-   end   
+   return Code == 200
 end
 
-function BasicAuth.requireAuthorization()
+function basicauth.requireAuthorization()
    net.http.respond{
       code=401,
       headers={["WWW-Authenticate"]='Basic realm=Channel Manager'}, 
       body="Please Authenticate"}
 end
 
-function BasicAuth.getCredentials(HttpMsg)
+function basicauth.getCredentials(HttpMsg)
    return getCreds(HttpMsg.headers)
 end
 
-return BasicAuth
+return basicauth
