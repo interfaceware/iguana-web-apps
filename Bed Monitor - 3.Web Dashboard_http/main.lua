6e8285d9b154747c92dd9c6626ea632df5686c76
eliot.muir <eliot.muir@interfaceware.com>
2014-05-10 07:24:09 -0400

    Simplified for new version of web server code.
    Added basicauth as a module.

--- Bed Monitor - 3.Web Dashboard_http/main.lua
+++ Bed Monitor - 3.Web Dashboard_http/main.lua
@@ -4,29 +4,15 @@ require 'lib.webserver'
 
 bedmonitor.db.init()
 
+local Server = lib.webserver.create{
+   actions = bedmonitor.actions,
+   default = 'app/bedmonitor/index.html',
+   -- If the test property is defined then static files are pulled from the sandbox 
+   -- rather than from the mile-stoned versioned copies of the files.  In production
+   -- the test propery should be commmented out.
+   --test = 'admin'    
+}
+
 function main(Data)
-   local Server = lib.webserver.create{
-      actions = bedmonitor.actions,
-      default = 'app/bedmonitor/index.html',
-      -- If the test property is defined then static files are pulled from the sandbox 
-      -- rather than from the mile-stoned versioned copies of the files.  In production
-      -- the test propery should be commmented out.
-      test = 'admin'    
-   }
-   
-   iguana.channelConfig{guid=iguana.channelGuid()}
-   
-   iguana.stopOnError(false) 
-   if iguana.isTest() then
-      Server:serveRequest{data=Data}
-   else
-      -- When running, push full stack error out to browser.
-      -- In the case of an internal error, log it.
-      local Stack = nil
-      local Success, ErrMsg = pcall(Server.serveRequest, Server, {data=Data})
-      if (not Success) then
-         local ErrObj = {error=ErrMsg}
-         net.http.respond{body=json.serialize{data=ErrObj}, entity_type='text/json'}
-      end
-   end
+   Server:serveRequest{data=Data}
 end
