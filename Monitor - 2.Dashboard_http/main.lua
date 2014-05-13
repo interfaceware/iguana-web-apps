57ac88a7c62c5a35f7f1d2cdd88d8ea9d2e0cb2d
unknown <eliot.muir@interfaceware.com>
2014-05-13 12:08:49 -0400

    Tidied up main function and got the
    New dependencies of basic auth and so on into the application

--- Monitor - 2.Dashboard_http/main.lua
+++ Monitor - 2.Dashboard_http/main.lua
@@ -5,17 +5,16 @@ require 'lib.webserver'
 -- The Data argument will contain the message to be processed.
 dash.init()
 
--- TODO - this code needs a little TLC to get to use the same framework as the channel
--- manager but it gets the concept across well enough.
+local Server = lib.webserver.create{
+   actions=dash.Actions,
+   default='app/monitor/index.html',
+   auth=true,
+   -- If the test property is defined then static files are pulled from the sandbox 
+   -- rather than from the mile-stoned versioned copies of the files.  In production
+   -- the test propery should be commmented out.
+   --test='admin'    
+}  
+
 function main(Data)
-   local Server = lib.webserver.create{
-      actions=dash.Actions,
-      default='app/monitor/index.html',
-      -- If the test property is defined then static files are pulled from the sandbox 
-      -- rather than from the mile-stoned versioned copies of the files.  In production
-      -- the test propery should be commmented out.
-      --test='admin'    
-   }  
    Server:serveRequest{data=Data}
-   iguana.stopOnError(false)   
 end
