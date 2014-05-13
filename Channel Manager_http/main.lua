a85b437c744c1e590014b69147e67012e0900849
unknown <eliot.muir@interfaceware.com>
2014-05-13 00:04:26 -0400

    Just added some comments about moving icon files.

--- Channel Manager_http/main.lua
+++ Channel Manager_http/main.lua
@@ -8,9 +8,13 @@ local Server = lib.webserver.create{
    -- If the test property is defined then static files are pulled from the sandbox 
    -- rather than from the mile-stoned versioned copies of the files.  In production
    -- the test property should be commented out.
-   -- test='admin'    
+   --test='admin'    
 }
 
 function main(Data)   
    Server:serveRequest{data=Data}
 end
+
+
+-- TODO - at some point we really should move the icon_* files into a shared library folder so they don't clutter up
+-- the top level directory of the other folder.  Not worth changing at this exact point in time.
