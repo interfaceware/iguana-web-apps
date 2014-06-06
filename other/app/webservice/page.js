4242c611723d9f3872c7f71716372ad5010becee
kevincai3 <kevin.cai@Kevins-MacBook-Pro.local>
2014-06-06 15:33:52 -0400

    Cleaned up code
    Refactor the code so the ajax call for helpdata is on the outside and
    can be called by other functions. Also added a bit of documentation.

--- other/app/webservice/page.js
+++ other/app/webservice/page.js
@@ -9,7 +9,8 @@ if (webservice === undefined) { var webservice = {}; }
 webservice.page = {}
    
 PAGE = webservice.page;
-   
+// This code may never be executed, but I'm not certain because it was here before I wrote my code
+// If it is till used, the CSS has been changed so the header and footer will not display properly.
 PAGE.functionHelp = function(Params){
    if (Params.call === undefined){
       webservice.help.showError("Please supply call &lt;function name&gt;")
@@ -21,16 +22,48 @@ PAGE.functionHelp = function(Params){
    });
 }
 
-   // lib.ajax.call('setHelp', Data, function(D){});
-
-// HACK FOR DEVELOPMENT
-var Tree2;
-
 webservice.state = {}
 
+function renderdatapage (Call){
+   lib.ajax.call('helpdata?call=' + Call, function(D){
+   console.log(D);
+   for (var key in D){
+      /* Key 1 = No function exists
+         Key 2 = No help for function exists
+         Key 3 = Help data returned
+      */
+      /* Currently, I coded it so that if no help for a function exists, it returns Key 3 with an empty JSON table .
+         However, since the helpsummary tree is built from a list of functions, the function is guranteed to exist
+         when called by helpdata, making Key 1 redundant. We may end up removing this whole Key system in the end.
+      */
+      if (key == 3) {
+         $('#helpdata').html(lib.help.render(D[key], Call));
+         $('#helpdata').ready(function(){
+            //Creates the help button
+            $('#helpdata').find('h1').first().before(
+               $('<button/>', {
+                  class: 'Edit',
+                  text: 'Edit',
+                  onClick: 'lib.help.togglemode(this)'
+            }));
+            //Applies the google-prettify to the appropriate fields (Usage and Examples)
+            $('#helpdata').find('pre').addClass('prettyprint lang-lua editable'); 
+            prettyPrint();
+         }); 
+      }
+      else {
+         //Returns error message (Currently it is impossible to get to this branch of the conditional statement unless
+         //the user decides to type in the URL directly.
+         $('#helpdata').html(webservice.help.header() + "<p>" + D[key] + "</p>" + webservice.help.footer());
+      }
+   }});
+}
+
+
 webservice.onBrowseTreeClick = function(Node){
+   //If the node clicked is a leaf and not a branch
    if (!Node.m_Children.length) {
-      //Returns the full node path
+      //Returns the full path of the function
       var Call = Node.m_Label;
       while (Node.m_Parent.m_Parent !== null) {
          Call = Node.m_Parent.m_Label + "." + Call;
@@ -38,34 +71,9 @@ webservice.onBrowseTreeClick = function(Node){
       }
       webservice.state.call = Call;
       // Makes the Ajax call
-      lib.ajax.call('helpdata?call=' + Call, function(D){
-         console.log(D);
-         for (var key in D){
-            /* Key 1 = No function exists
-               Key 2 = No help for function exists
-               Key 3 = Help data returned
-            */
-            if (key == 3) {
-               $('#helpdata').html(lib.help.render(D[key], Call));
-               $('#helpdata').ready(function(){
-                  //Creates the help button
-                  $('#helpdata').find('h1').first().before(
-                     $('<button/>', {
-                        class: 'Edit',
-                        text: 'Edit',
-                        onClick: 'lib.help.togglemode(this)'
-                  }));
-                  //Applies classes to decorate code
-                  $('#helpdata').find('pre').addClass('prettyprint lang-lua editable'); 
-                  prettyPrint();
-               }); 
-            }
-            else {
-               $('#helpdata').html(webservice.help.header() + "<p>" + D[key] + "</p>" + webservice.help.footer());
-            }
-         }
-      });
+      renderdatapage(Call);
    }
+   //If the node clicked is a branch
    else{
       Node.toggle();
    }
@@ -75,11 +83,9 @@ PAGE.browse = function(Params) {
    lib.ajax.call('helpsummary', function(D){
       console.log(D);
       $('body').html("<div id='browser'></div><div id='helpdata'></div>");
-      $.each(D, function(key,value){
-         console.log(value);
-         Tree2 = new Tree22(key, "tree");
-         myRender(value, Tree2);
-      });
+      
+      var Tree2 = new Tree22('highrise', "tree");
+      myRender(D, Tree2);
       Tree2.render($("#browser"));
       Tree2.setOnClick(webservice.onBrowseTreeClick);
       Tree2.open();
@@ -87,7 +93,8 @@ PAGE.browse = function(Params) {
 }
 
 PAGE.default = PAGE.browse
-   
+
+// This function can be put into page.js as a method of the Tree22 object   
 function myRender(D, tree){
   $.each(D, function(key, value){
    if (!tree.IsOpen){
