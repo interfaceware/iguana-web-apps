9478a9bc01e21b755dffd31950c6217243953519
wshrews <wade.shrewsbury@interfaceware.com>
2014-06-12 15:49:41 -0400

    Added config of servers.
    Switched over to using datatables.

--- other/app/testrunner/page.js
+++ other/app/testrunner/page.js
@@ -2,6 +2,7 @@ $(document).ready(function($) {
    console.log('Load page handling framework.');
    console.log(testrunner);
    lib.page.init(testrunner.page);
+   // TODO get rid of using lib/ajax.js and swtich over to how the channel manager does it.
    lib.ajax.errorFunc = testrunner.help.showError;
 });
 
@@ -12,11 +13,138 @@ PAGE=testrunner.page;
   
 PAGE.main = function() {
    var H = testrunner.help.header();
-  
-   H += '<p>This is the Test Runner GUI. <a href="#Page=run&test=FileRun">Click here</a> to execute the tests.</p>';
-
-   H +=  testrunner.help.footer();
-   $('body').html(H);
+   lib.ajax.call('start', function(R){
+      if(typeof R.IsSetup != 'undefined' && !R.IsSetup){
+         H += "<p>It looks like you haven't configured this app yet. Please set your initial configuration in testrunner.ini and then <a href='#Page=config'>continue</a>.</p>" ;
+         H += "<p>If you have already finished configuring then <a href=\"#Page=run\">executing your first run</a> will prevent this page from appearing in the future.</p>";
+         H += testrunner.help.footer();
+         $('body').html(H);
+      } else {
+         H += "<table id=\"results_table\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"></table>";
+         H += "<a href=\"#Page=run\">Run Tests</a><br /><a href=\"#Page=config\">Manage Configuration</a>"
+         H +=  testrunner.help.footer();
+         $('body').html(H);
+         $("#results_table").dataTable(R);
+      }
+   });
+}
+   
+PAGE.config = function(){
+   lib.ajax.call('list', function(R){
+      var H = testrunner.help.header();
+      H += "<h3 style=\"margin-bottom: -20px;\">Test Hosts</h3>";
+      H += "<table id=\"hosts\">";
+      H += testrunner.help.hostTableHead();
+      for(var key in R){
+         H += "<tr>";
+         H += "<td></td>";
+         H += "<td>" + key + "</td>";
+         H += "<td>" + R[key]['host'] + "</td>";
+         H += "<td>" + R[key]['https'] + "</td>";
+         H += "<td>" + R[key]['port'] + "</td>";
+         H += "<td>" + R[key]['http_port'] + "</td>";
+         H += "<td>" + R[key]['user'] + "</td>";
+         H += "<td>" + R[key]['pass'] + "</td>";
+         H += "</tr>";
+      }
+      H += "<table>";
+      H += "<a href=\"#Page=configEdit\">Edit & Add Hosts</a>";
+      H += "<br /><a href=\"#Page=run\">Run Tests</a>";
+      H += "<br /><a href=\"#Page=main\">Home</a>";
+      H += testrunner.help.footer();
+      $('body').html(H);
+   });
+}
+   
+PAGE.configEdit = function(){
+   lib.ajax.call('list', function(R){
+      var H = testrunner.help.header();
+      H += "<form id=\"hosts\" action=\"save\"><table id=\"hosts\">";
+      H += testrunner.help.hostTableHead();
+      var count = 0;
+      for(var key in R){
+         count++
+         H += "<tr>";
+         H += "<td><input name=\"host_id\" type=\"hidden\" value=\"" + R[key]['host_id'] + "\"/>";
+         H += "<td><input name=\"name\" type=\"text\" value=\"" + key + "\" /></td>";
+         H += "<td><input name=\"host\" type=\"text\" value=\"" + R[key]['host'] + "\" /></td>";
+         H += "<td><input name=\"https\" type=\"text\" value=\"" + R[key]['https'] + "\" /></td>";
+         H += "<td><input name=\"port\" type=\"text\" value=\"" + R[key]['port'] + "\" /></td>";
+         H += "<td><input name=\"http_port\" type=\"text\" value=\"" + R[key]['http_port'] + "\" /></td>";
+         H += "<td><input name=\"user\" type=\"text\" value=\"" + R[key]['user'] + "\" /></td>";
+         H += "<td><input name=\"pass\" type=\"text\" value=\"" + R[key]['pass'] + "\" /></td>";
+         H += "<td><input name=\"results\" type=\"hidden\" value=\"" + R[key]['results'] + "\" /></td>";
+         H += "<td><span class=\"delete\">Delete</span></td>";
+         H += "</tr>";
+      }
+      if(count <= 0){
+         H += "<tr>";
+         H += "<td><input name=\"host_id\" type=\"hidden\" value=\"\"/>";
+         H += "<td><input name=\"name\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"host\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"https\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"port\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"http_port\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"user\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"pass\" type=\"text\" value=\"\" /></td>";
+         H += "<td><input name=\"results\" type=\"hidden\" value=\"\" /></td>";
+         H += "<td><span class=\"delete\">Delete</span></td>";
+         H += "</tr>";
+      }
+      H += "</tbody></table></form>";
+      H += "<span id=\"add\">Add Host</span> | ";
+      H += "<span id=\"save\">Save Hosts</span> | ";
+      H += "<span id=\"cancel\">Cancel</span>";
+      H += testrunner.help.footer();
+      $('body').html(H);
+      $('#add').click(function () {
+         $('#hosts tbody tr:last').clone(true).insertAfter('#hosts tbody>tr:last');
+         $('#hosts tbody tr:last input').val('');
+         return false;
+      });
+      $('.delete').click(function() {
+         var tr = $(this).closest('tr');
+         if ($('#hosts tbody tr').length > 1) {
+            tr.remove();
+         } else {
+            $('#hosts tbody tr:last').clone(true).insertAfter('#hosts tbody>tr:last');
+            $('#hosts tbody tr:last input').val('');
+            tr.remove();
+         }
+         return false;
+      });
+      $('#save').click(function() {
+         var rows = [];
+         $('#hosts tbody tr').each(function(){
+            rows.push({
+               host_id: $(this).find('input[name="host_id"]').val(),
+               name: $(this).find('input[name="name"]').val(),
+               host: $(this).find('input[name="host"]').val(),
+               https: $(this).find('input[name="https"]').val(),
+               port: $(this).find('input[name="port"]').val(),
+               http_port: $(this).find('input[name="http_port"]').val(),
+               user: $(this).find('input[name="user"]').val(),
+               pass: $(this).find('input[name="pass"]').val(),
+            });
+         });
+         console.log(rows)
+         $.ajax({
+            url: 'save',
+            dataType: 'json',
+            type: 'POST',
+            data: JSON.stringify(rows),
+            success: function(data){
+               if(data[0]===true) {
+                  window.location.hash = "#Page=config";
+               }
+            }
+         });
+         return false;
+      });
+      $("#cancel").click(function() {
+         window.location.hash = "#Page=config";
+      });
+   });
 }
    
 PAGE.run = function(R){
@@ -24,20 +152,10 @@ PAGE.run = function(R){
    H += '<p>Running tests...</p>'
    H +=  testrunner.help.footer(); 
    $('body').html(H);
-   
    lib.ajax.call('run', function(R){
-      var R = R;
-      var H = testrunner.help.header(); 
-      for (var i = 0; i < R.length; i++){
-         H += "<h2 style=\"color: black; border-bottom: 1px dotted black; padding: 10px; margin: 10px 0px 10px 0px;\">Results for " + R[i][0]["name"] + " (" + R[i][0]["os"] + ")</h2>"
-         for (var v in R[i][1]) {
-            H += v + ": " + R[i][1][v] + "<br>";
-         }
-      }
-      H += '<a href="#Page=main">&lt; Home</a>';
-      H +=  testrunner.help.footer();      
-     $('body').html(H);
-
+      var Result = R
+      window.location.hash = '#Page=main'
    });
 }  
+
 PAGE.default = PAGE.main;
