9fa18a752cdc400253dc8a7673b34c30ef3a7450
wshrews <wade.shrewsbury@interfaceware.com>
2014-06-12 15:45:32 -0400

    Change terminology to use main instead of dashboard.
    IE8 compatibility change and put in header for server config.

--- other/app/testrunner/help.js
+++ other/app/testrunner/help.js
@@ -1,4 +1,4 @@
-if (!testrunner) { testrunner = {}; }
+if (typeof testrunner == "undefined") { testrunner = {}; }
 
 testrunner.help = {};
    
@@ -9,12 +9,16 @@ testrunner.help.header = function(){
 testrunner.help.footer = function(){
    return '</div>';
 }
+   
+testrunner.help.hostTableHead = function() {
+   return "<thead><tr>&nbsp;<th></th><th>Name</th><th>Host</th><th>HTTPS</th><th>Port</th><th>HTTP Port</th><th>User</th><th>Pass</th><th>&nbsp;</th></tr></thead><tbody>";
+}
 
 testrunner.help.breadCrumb = function(T){
-   return "<hr><a href='#'>Dashboard</a> &gt; "+ T +"<hr>";
+   return "<hr><a href='#'>Main</a> &gt; "+ T +"<hr>";
 }
 
 testrunner.help.showError = function(Msg){
-   $('body').html(testrunner.help.header() + "<font color='red'>" + Msg + '</font> <p><a href="#">Back to dashboard</a>' + testrunner.help.footer());
+   $('body').html(testrunner.help.header() + "<font color='red'>" + Msg + '</font> <p><a href="#">Back to Main</a>' + testrunner.help.footer());
 }
 
