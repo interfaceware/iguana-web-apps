39b4c075526a4ca5956ffa962cc5ac8c1faafabc
kevincai3 <kevin.cai@Kevins-MacBook-Pro.local>
2014-06-23 11:09:33 -0400

    Added ability to download and manage github

--- shared/cm/githelper.lua
+++ shared/cm/githelper.lua
@@ -1,9 +1,42 @@
 if not cm then cm = {} end
 if not cm.githelper then cm.githelper = {} end
 
---local filez = {one = "fake", two = "superfake", three = "powerfake"}
+local function comparedates(base, new)
+   for i, v in ipairs(base) do
+      if v < new[i] then
+         return true
+      elseif v > new[i] then
+         return false
+      end
+   end
+end
 
-local function writemd5(path, checksum)
+function cm.githelper.comparecommits(committree, root, remotesrc)
+   local topcommit = {}
+   for i, v in ipairs(committree) do
+      if next(topcommit) == nil  or comparedates(topcommit.date:split('%D'), committree[i].commit.author.date:split('%D')) then
+         topcommit = {data = v, date = committree[i].commit.author.date}
+      end
+   end
+   trace(topcommit)
+   if os.fs.access(root..'lastsha') and os.fs.readFile(root..'lastsha') == topcommit.data.sha then
+      return 
+   end
+   local zipdata, statuscode = net.http.get{url=remotesrc, live=true}
+   if statuscode >= 400 then
+        return {link=remotesrc, err="Bad URL. Error "..statuscode}
+   end
+   local tree=filter.zip.inflate(zipdata)
+   for k,v in pairs(tree) do
+      tree = v
+   end  
+   os.fs.cleanDir(root)
+   os.fs.writeFile(root..'lastsha', topcommit.data.sha, 666)
+   cm.app.WriteFiles(root, tree)
+   return 
+end  
+
+--[[local function writemd5(path, checksum)
    local F = io.open(path, 'wb+')
    F:write(checksum)
    F:close()
@@ -27,12 +60,12 @@ function cm.githelper.verifychanges(filetree, path, checksum)
 end
 
 function cm.githelper.writefiles(filetree, destination)
---[[for key, val in pairs(filez) do 
+for key, val in pairs(filez) do 
       trace(key)
       local sample = io.open('/Users/kevin.cai/Desktop/' .. val, 'w+')
       sample:write("HELLO")
       sample:close()
-   end]]    
+   end  
    for k,v in pairs(filetree) do
       local path = os.fs.name.toNative(destination .. '/' .. k)
       trace(path)
@@ -50,4 +83,4 @@ function cm.githelper.writefiles(filetree, destination)
          F:close()         
       end
    end
-end
+end]]
