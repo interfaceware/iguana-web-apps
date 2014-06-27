f01018b27dffdf71022a422bc39551a6803aab44
eliot.muir <eliot.muir@interfaceware.com>
2014-06-27 14:42:04 -0400

    Updated DBS example to fit with new API.
    This will exist in 5.6.8.

--- New DBS File Format Example_to/main.lua
+++ New DBS File Format Example_to/main.lua
@@ -1,23 +1,26 @@
 -- This script maps specific patient data to an external database.
+-- It makes use of the example DBS file format which was introduced
+-- in Iguana 5.6.8.  If you have an earlier version of Iguana then
+-- this won't work.
 
 -- Connect to database
 DB = db.connect{api=db.SQLITE, name='Demo/PatientData.sqlite'}
 
-local DbSchema = dbs.tableLayoutFromFile{filename='todatabase/example.dbs'}
-   
+-- This dbs file support requires Iguana 5.6.8 or above.
+local DbSchema = dbs.init{filename='todatabase/example.dbs'}
+
 function main(Data)
-   
    -- Parse incoming raw message with hl7.parse
-   local MsgIn = hl7.parse{vmd='example/demo.vmd', data=Data}
-   
-   -- Build node tree of table to temporarily store information
-   -- tables.vmd name = 'ADT'
-   local TableOut = DbSchema:getTableSet('ALL')
+   local MsgIn = hl7.parse{vmd='example/demo.vmd',
+                          data=Data}
+ 
+   -- Let's just get all the tables.
+   local TableOut = DbSchema:tables()
    
    -- Map fields from PID segment to Patient table
    MapPatient(TableOut.Patient[1], MsgIn.PID)
    
-   -- Insert information from TableOut into the real table
+   -- Insert information from TableOut into the real DB
    DB:merge{data=TableOut, live=false}
    -- Change the above live=false to live=true 
    
