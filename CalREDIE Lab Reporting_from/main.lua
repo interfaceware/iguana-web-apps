a727ea0fcb08f057be554e1396fd34ae5163e977
wshrews <wade.shrewsbury@interfaceware.com>
2014-12-17 13:17:38 -0500

    Added CalREDIE Lab Reporting channel

---
+++ CalREDIE Lab Reporting_from/main.lua
@@ -0,0 +1,7 @@
+-- CalREIDE message is sent in the To Translator
+-- This is empty at the moment.  You'll need to hook this up to a real data source like your HIS.
+-- If you use Paragon let us know since we have some other users that use Paragon and may be able
+-- to give you some mappings for that HIS system.
+
+function main()
+end
